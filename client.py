3c138066854c90e708df754078d0b4d45554b5bf
Glit <vpad.lazar.04@gmail.com>
2024-12-01 22:13:31 +0200

    A small password regex fix

--- client.py
+++ client.py
@@ -21,7 +21,7 @@ class Client:
     @staticmethod
     def is_valid_password(password):
         #Checks if the password is valid
-        return bool(re.match("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&-+=()])(?=\\S+$).{8,20}$", password))
+        return bool(re.match("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&-+=()])(?=\S+$).{8,20}$", password))
 
     @staticmethod
     def is_valid_email(email):
@@ -31,7 +31,7 @@ class Client:
     @staticmethod
     def get_public_key():
         #Gets the public RSA key from the server
-        key_response = requests.get("https://localhost:5000/get_public_key", verify=False)
+        key_response = requests.get(f"{server_url}/auth/get_public_key", verify=False)
         public_key = RSA.import_key(key_response.json()['public_key'])
         cipher = PKCS1_OAEP.new(public_key)
         return cipher
@@ -46,7 +46,7 @@ class Client:
         encrypted_password = base64.b64encode(cipher.encrypt(password.encode())).decode()
         payload = {"username": encrypted_username, "password": encrypted_password}
         try:
-            response = requests.post(f"{server_url}/login", json=payload, verify=False)
+            response = requests.post(f"{server_url}/auth/login", json=payload, verify=False)
             if response.status_code == 200:
                 user_id = response.json()["id"]
                 return user_id
@@ -66,7 +66,7 @@ class Client:
         encrypted_password = base64.b64encode(cipher.encrypt(password.encode())).decode()
         payload = {"name": name, "username": encrypted_username, "password": encrypted_password, "email": email}
         try:
-            response = requests.post(f"{server_url}/register", json=payload, verify=False)
+            response = requests.post(f"{server_url}/auth/register", json=payload, verify=False)
             if response.status_code == 201:
                 return True
             else:
@@ -83,7 +83,7 @@ class Client:
             with open(file_path, "rb") as f:
                 content = f.read()
             payload = {"created_by": str(user_id), "creator_type": "user", "name": name}
-            response = requests.post(f"{server_url}/upload", data=payload, files={"file": content}, verify=False)
+            response = requests.post(f"{server_url}/content/upload", data=payload, files={"file": content}, verify=False)
             if response.status_code == 201:
                 return True
             else:
@@ -95,7 +95,7 @@ class Client:
     def refresh_content():
         #Refreshed the list of content available
         try:
-            response = requests.get(f"{server_url}/content", verify=False)
+            response = requests.get(f"{server_url}/content/refresh", verify=False)
             return response.json()
         except Exception as e:
             raise Exception("An error occurred: " + str(e))
@@ -105,7 +105,7 @@ class Client:
         #Downloads the content from the server
         content_id = content.get("id")
         try:
-            response = requests.get(f"{server_url}/download/{content_id}", stream=True, verify=False)
+            response = requests.get(f"{server_url}/content/download/{content_id}", stream=True, verify=False)
             if response.status_code == 200:
                 return response.iter_content(chunk_size=8192)
             else:
